01c566069115d6f3213ab97fe371881bcde17273
Артем <pakshaartem@gmail.com>
2025-05-19 17:41:51 +0300

    fix

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { register } from "../controllers/authController";
+import { Request, Response, NextFunction } from "express";
 
 const authRoutes = express.Router();
 
-authRoutes.post("/register", (req, res, next) => {
+authRoutes.post("/register", (req: Request, res: Response, next: NextFunction) => {
     register(req, res).catch(next);
 });
 
