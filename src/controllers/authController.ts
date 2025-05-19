fbefd4b3813e383ddcb476841d0b386f94deb6cd
Артем <pakshaartem@gmail.com>
2025-05-20 02:16:07 +0300

    fix

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -20,7 +20,7 @@ export const register = async (req: Request, res: Response): Promise<Response> =
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const defaultPhotoPath = path.join(__dirname, "public/assets/default-avatar.png");
+    const defaultPhotoPath = path.join(__dirname, "../../public/assets/default-avatar.png");
     const photoBuffer = fs.readFileSync(defaultPhotoPath);
 
     const newUser = new User({
