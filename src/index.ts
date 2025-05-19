65d7af2b3b5505d4f1dbce77c48c20ba0dfab327
Артем <pakshaartem@gmail.com>
2025-05-19 18:03:45 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db";
 import authRoutes from "./routers/auth";
+import cors from "cors";
+
 
 dotenv.config();
 
@@ -17,6 +19,11 @@ app.use("/api", authRoutes);
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true 
+}));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
