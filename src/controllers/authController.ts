dcf068a90a508805442ca064d58e27a813fa5925
Артем <pakshaartem@gmail.com>
2025-05-19 18:17:10 +0300

    fix

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -31,6 +31,13 @@ export const register = async (req: Request, res: Response): Promise<Response> =
 
     const token = jwt.sign({ id: newUser._id }, JWT_SECRET, { expiresIn: "7d" });
 
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
     return res.status(201).json({
       message: "Пользователь успешно зарегистрирован",
       token,
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import connectDB from "./database/db";
 import authRoutes from "./routers/auth";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
 dotenv.config();
@@ -11,19 +12,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-
+app.use(cookieParser());
 connectDB();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true 
+}));
 
 app.use("/api", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-
-app.use(cors({
-    origin: "http://localhost:3000",
-    credentials: true 
-}));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
