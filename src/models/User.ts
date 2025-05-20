0732bf3af3d8ea01663883486cc08d9bb5a7ca1e
Артем <pakshaartem@gmail.com>
2025-05-20 12:16:29 +0300

    f

--- src/models/User.ts
+++ src/models/User.ts
@@ -26,6 +26,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    gender: {
+        type: String,
+    },
     }, 
     {
         timestamps: true,
